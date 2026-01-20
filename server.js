59294d60d3dbfbf510568cd416caaa4f77c4dae4
itsme-vivekthecoder <vivekthecoder2211@gmail.com>
2026-01-20 10:08:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const crypto = require("crypto");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://shanti-palace-vrindavan.netlify.app",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
+;
 app.use(express.json());
 
 const razorpay = new Razorpay({
@@ -64,3 +69,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
